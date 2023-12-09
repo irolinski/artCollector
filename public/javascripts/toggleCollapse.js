c91267f8fb25a9b2c3c344975530150fa7f359fe
szczek <ignacy.rolinski@gmail.com>
2023-12-10 00:31:10 +0100

    more show page styling + animation/transformation scripts

---
+++ public/javascripts/toggleCollapse.js
@@ -0,0 +1,8 @@
+
+const toggleCollapse = (inputClass) => {
+    for (el of document.querySelectorAll(inputClass)) 
+    { if (!el.classList.contains('show')) {
+      el.classList.add('show') } else {
+        el.classList.remove('show')
+      }}}
+  
---
+++ public/javascripts/transform_imgOrientation.js
@@ -0,0 +1,85 @@
+check_imgOrientation = function () {
+
+    const images = document.querySelectorAll('img');
+
+    for (let i = 0; i < images.length; i++) {
+        if (images[i].width > images[i].height) {
+            images[i].parentElement.classList.add('landscape')
+        } else {
+            images[i].parentElement.classList.add('portrait')
+        }
+    }
+};
+
+
+
+const carousel = document.querySelector('#carousel');
+
+
+window.onload = () => {
+
+    let activeItem = document.querySelector('.carousel-item.active');
+
+
+    check_imgOrientation();
+
+    activeItem.classList.add('first-img');
+    document.querySelector('.carousel-inner').lastElementChild.classList.add('last-img')
+
+    if (activeItem.classList.contains('landscape')) {
+        carousel.classList.add('drop');
+    } else {
+        carousel.classList.remove('drop')
+    }
+}
+
+
+
+
+
+
+let btn_check_landscape_next = () => {
+
+    const firstImg = document.querySelector('.first-img');
+    let activeItem = document.querySelector('.carousel-item.active');
+
+    if (activeItem.nextElementSibling !== null) {
+
+        if (activeItem.nextElementSibling.classList.contains('landscape')) {
+            carousel.classList.add('drop');
+        } else {
+            carousel.classList.remove('drop');
+        }
+    } else {
+        if (firstImg.classList.contains('landscape')) {
+            carousel.classList.add('drop')
+        } else {
+            carousel.classList.remove('drop');
+        }
+    }
+
+
+};
+
+let btn_check_landscape_prev = () => {
+
+    const lastImg = document.querySelector('.last-img');
+    let activeItem = document.querySelector('.carousel-item.active');
+
+    if (activeItem.previousElementSibling !== null) {
+
+        if (activeItem.previousElementSibling.classList.contains('landscape')) {
+            carousel.classList.add('drop');
+        } else {
+            carousel.classList.remove('drop');
+        }
+    } else {
+        if (lastImg.classList.contains('landscape')) {
+            carousel.classList.add('drop');
+        } else {
+            carousel.classList.remove('drop');
+        }
+    }
+
+
+};
