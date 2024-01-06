d7f2ee0a037d02334114bc6736e0b74306bdf2e5
szczek <ignacy.rolinski@gmail.com>
2024-01-06 12:02:01 +0100

    fix(bootstrap_validation): show cursor: wait only when the form has passed the validations fix(forms): add missing minlength and maxlength attributes

--- public/javascripts/bootstrap_validations.js
+++ public/javascripts/bootstrap_validations.js
@@ -1,3 +1,11 @@
+addEventListener("pageshow", () => {
+    if (document.body.classList.contains('wait')){
+        document.body.classList.remove('wait')
+    }
+});
+
+const cursorWait = () => document.body.classList.add('wait');
+
 (function () {
 
     let forms = document.querySelectorAll('.needs-validation')
@@ -6,22 +14,19 @@
     // Loop over them and prevent submission
     Array.prototype.slice.call(forms)
         .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-            if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-            if (prevButtonNewForm){
-                prevButtonNewForm.click()}
-                
-            }
-    
-            form.classList.add('was-validated')
-        }, false)
-        })
-
-
-    }
-    
-    )()
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+                if (prevButtonNewForm){
+                    prevButtonNewForm.click()}
+                    
+                } else {
+                    cursorWait();
+                }
+                form.classList.add('was-validated');
+            }, false)
+        });
+})();
 
     
--- public/javascripts/username_validation.js
+++ public/javascripts/username_validation.js
@@ -4,7 +4,7 @@ let username = document.getElementById('username-register');
 
 function validateUsername() {
   if(username.value.length < 4 || username.value.length >= 12) {
-    username.setCustomValidity('Username needs to be longer than 4 characters and (shorter than 12)')
+    username.setCustomValidity('Username needs to be longer than 4 characters (and shorter than 12)')
   } else {
     username.setCustomValidity('');
   }
